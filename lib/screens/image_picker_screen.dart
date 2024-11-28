971570a51c5b7a9de296d8db6b68e3a827da2151
KabaDiakite0212 <mouslimibnislam2021@gmail.com>
2024-11-28 10:31:45 +0000

    multi select files

--- lib/screens/image_picker_screen.dart
+++ lib/screens/image_picker_screen.dart
@@ -16,6 +16,7 @@ class ImagePickerScreen extends StatefulWidget {
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
   File? selectedImage;
   File? selectedImageCamera;
+  List<File>? mutliSelectedImage;
 
   @override
   Widget build(BuildContext context) {
@@ -28,81 +29,126 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BasicWidgets.heightSize(20),
-            InkWell(
-              onTap: () async {
-                var imageGallery =
-                    await ImagePicker().pickImage(source: ImageSource.gallery);
-                setState(() {
-                  selectedImage = File(imageGallery!.path);
-                });
-              },
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                height: 300,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(20)),
-                child: selectedImage != null
-                    ? ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.file(
-                          selectedImage!,
-                          height: 300,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                    )
-                    : const Center(
-                        child: CustomText(
-                          title: "Image - Gallerie",
-                        ),
-                      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // BasicWidgets.heightSize(20),
+              // InkWell(
+              //   onTap: () async {
+              //     var imageGallery =
+              //         await ImagePicker().pickImage(source: ImageSource.gallery);
+              //     setState(() {
+              //       selectedImage = File(imageGallery!.path);
+              //     });
+              //   },
+              //   child: Container(
+              //     margin: const EdgeInsets.all(10),
+              //     height: 300,
+              //     width: double.infinity,
+              //     decoration: BoxDecoration(
+              //         color: Colors.grey.shade300,
+              //         borderRadius: BorderRadius.circular(20)),
+              //     child: selectedImage != null
+              //         ? ClipRRect(
+              //             borderRadius: BorderRadius.circular(20),
+              //             child: Image.file(
+              //               selectedImage!,
+              //               height: 300,
+              //               width: double.infinity,
+              //               fit: BoxFit.cover,
+              //             ),
+              //           )
+              //         : const Center(
+              //             child: CustomText(
+              //               title: "Image - Gallerie",
+              //             ),
+              //           ),
+              //   ),
+              // ),
+              // BasicWidgets.heightSize(20),
+              // InkWell(
+              //   onTap: () async {
+              //     var imageCamera =
+              //         await ImagePicker().pickImage(source: ImageSource.camera);
+              //     setState(() {
+              //       selectedImageCamera = File(imageCamera!.path);
+              //     });
+              //   },
+              //   child: Container(
+              //     margin: const EdgeInsets.all(10),
+              //     height: 300,
+              //     width: double.infinity,
+              //     decoration: BoxDecoration(
+              //         color: Colors.grey.shade300,
+              //         borderRadius: BorderRadius.circular(20)),
+              //     child: selectedImageCamera != null
+              //         ? ClipRRect(
+              //             borderRadius: BorderRadius.circular(20),
+              //             child: Image.file(
+              //               selectedImageCamera!,
+              //               height: 300,
+              //               width: double.infinity,
+              //               fit: BoxFit.cover,
+              //             ),
+              //           )
+              //         : const Center(
+              //             child: CustomText(
+              //               title: "Image - Camera",
+              //             ),
+              //           ),
+              //   ),
+              // ),
+              BasicWidgets.heightSize(5),
+              ElevatedButton(
+                onPressed: () async {
+                  List<XFile> multiImage =
+                      await ImagePicker().pickMultiImage(limit: 3);
+
+                  List<File> fileMapped =
+                      multiImage.map((file) => File(file.path)).toList();
+                  setState(() {
+                    mutliSelectedImage = fileMapped;
+                  });
+                },
+                child: CustomText(
+                  title: "Select MultiImage",
+                ),
               ),
-            ),
-           
-            BasicWidgets.heightSize(20),
-            InkWell(
-              onTap: () async {
-                var imageCamera =
-                    await ImagePicker().pickImage(source: ImageSource.camera);
-                setState(() {
-                  selectedImageCamera = File(imageCamera!.path);
-                });
-              },
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                height: 300,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(20)),
-                child: selectedImageCamera != null
-                    ? ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.file(
-                          selectedImageCamera!,
-                          height: 300,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
+              BasicWidgets.heightSize(20),
+
+              mutliSelectedImage != null
+                  ? SizedBox(
+                      height: MediaQuery.of(context).size.height / 1.2,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                          itemCount: mutliSelectedImage!.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                margin: const EdgeInsets.all(10),
+                                height: 300,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade300,
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.file(
+                                    mutliSelectedImage![index],
+                                    height: 300,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ));
+                          }),
                     )
-                    : const Center(
-                        child: CustomText(
-                          title: "Image - Camera",
-                        ),
-                      ),
-              ),
-            ),
-           
-            BasicWidgets.heightSize(20),
-          ],
+                  : const SizedBox()
+            ],
+          ),
         ),
       ),
     );
